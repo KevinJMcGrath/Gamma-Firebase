c86406dbddb5b4abc05a1424a3fef09cf19cd44a
antiproton85 <kevinmcgr@gmail.com>
2017-11-03 15:21:50 -0400

    Elements-UI experiments

--- public/js/register-vue.js
+++ public/js/register-vue.js
@@ -1,12 +1,22 @@
+//Getting started sample:
+//https://jsfiddle.net/hzfpyvg6/14/
+
 var dataObj = {
 	items: [
 		{ datum: "Test 1" },
 		{ datum: "Test 2" },
 		{ datum: "Test 3" }
-	]	
+	],
+	fftest: "Hello",
+	button: {
+		label: "Click Me"
+	},
+	verifyemail: ""	
 }
 
-var vm = new Vue({
-	el: '#testDiv',
-	data: dataObj
-});
+var vmForElements = {	
+	data: function() { return dataObj }
+};
+
+var vm = Vue.extend(vmForElements);
+new vm().$mount('#testDiv');
