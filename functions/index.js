d9d7c61be43474b86a3ebc774de36234691abbad
antiproton85 <kevinmcgr@gmail.com>
2017-11-03 11:24:04 -0400

    Added test addMessage function

--- functions/index.js
+++ functions/index.js
@@ -5,3 +5,11 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+exports.addMessage = functions.https.onRequest((req, res) => { 
+	//Grab text param
+	const original = req.query.text;
+
+	console.log('Submitted text: ' + original);
+
+	res.send({ status: 'success'});
+});
